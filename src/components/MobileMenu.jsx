3f8865cb689b76b5e5a93b982c8639ca38a984dc
Laur <laur_laurentiu2013@yahoo.com>
2023-02-13 16:09:35 +0200

    create funtionality for the mobile sidebar button, improve ux on search results mobile layout

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -1,22 +1,40 @@
 import { RiCloseLine } from 'react-icons/ri';
 import { HiOutlineMenu } from 'react-icons/hi';
-import { useState } from 'react';
-import SideBar from './SideBar';
+import { useState, useEffect } from 'react';
+import SideTile from './SideTile';
+import { genresData } from './SideBar';
 
 const MobileMenu = () => {
 
     const [mobileMenu, setMobileMenu] = useState(false);
+    const [genres, setGenres] = useState();
+
+    useEffect(() => {
+        setGenres(prevState => {
+            return genresData.genres.map((genre) => {
+                return <SideTile
+                        key={genre.id}
+                        genre={genre}/>
+            })
+        })
+    }, [])
     
     return(
-        <div className="absolute md:hidden block top-6 right-3">
+        <div className="relative md:hidden block left-[0.35rem]">
             {mobileMenu ?
+                <>
                 <RiCloseLine className='w-6 h-6 text-white mr-2' onClick={() => setMobileMenu(false)} />
+                    <div className="bg-neutral-800 text-neutral-100 p-2 pr-0 h-auto drop-shadow-lg">
+                        <ul className="cursor-pointer text-[0.99rem] space-y-2 w-[140px]">
+                            {genres}
+                        </ul>
+                    </div>
+                </>
                 :
                 <HiOutlineMenu className='w-6 h-6 text-white mr-2' onClick={() => setMobileMenu(true)} />
             }
         </div>
     )
-
 
 }
 
--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -4,6 +4,8 @@ import MovieCard from "../components/MovieCard";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import apiKey from "../assets/API";
+import {motion} from 'framer-motion'
+
 
 const SearchResults = () => {
 
@@ -25,18 +27,20 @@ const SearchResults = () => {
 
        <div className="flex flex-col">
 
-            <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10"> Showing results for <span className="font-white" >{searchTerm}</span> </h2>
+            <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10 ml-4"> Showing results for <span className="font-white" >{searchTerm}</span> </h2>
 
-            <div className="flex gap-4 ml-4">
-                    {movies.map((movie, i) => {
-                        return (
-                            <MovieCard
-                                key={i}
-                                {...movie}
-                            />
-                        )
-                    })}
-            </div>
+            <motion.div drag='x' dragElastic={0.7} dragConstraints={{right:0, left:-600}}>
+                <div className="flex gap-4 ml-4 h-[100vh]">
+                        {movies.map((movie, i) => {
+                            return (
+                                <MovieCard
+                                    key={i}
+                                    {...movie}
+                                />
+                            )
+                        })}
+                </div>
+            </motion.div>
 
        </div>
 
